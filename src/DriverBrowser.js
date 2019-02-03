16e3f8c381fb0a2124ae83d7b901e756ba1a46c6
Filipe Borges <filipebkc2209@gmail.com>
2019-02-03 09:57:14 -0200

    Implemented DriverBrowser

---
+++ src/DriverBrowser.js
@@ -0,0 +1,55 @@
+import { Builder, Browser } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/firefox';
+// import Logger from './log/Logger';
+import getRandomNumber from './util/NumberUtils';
+
+class DriverBrowser {
+  constructor() {
+    process.env.PATH = `${process.env.PATH}:${process.env.BROWSER_DRIVER_PATH}`;
+
+    const firefoxOptions = new Options();
+    firefoxOptions
+      .setBinary(process.env.BROWSER_BIN_PATH)
+      .headless();
+
+    this.driver = new Builder()
+      .forBrowser(Browser.FIREFOX)
+      .setFirefoxOptions(firefoxOptions)
+      .build();
+  }
+
+  $getHtmlViaGetRequest(url) {
+    return this.driver.get(url)
+      .then(() => (
+        this.driver.getPageSource()
+          .catch((err) => {
+            // Logger.logError(err);
+            console.log(err);
+            throw err;
+          })
+      ))
+      .catch((err) => {
+        // Logger.logError(err);
+        console.log(err);
+        throw err;
+      });
+  }
+
+  retriveHtmlWithDelay(url) {
+    return (
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, getRandomNumber(0, 7000));
+      })
+    ).then(() => this.$getHtmlViaGetRequest(url));
+  }
+
+  quit() {
+    this.driver.quit();
+  }
+}
+
+const driverBrowser = new DriverBrowser();
+
+export default driverBrowser;
