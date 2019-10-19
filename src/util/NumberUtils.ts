fc2c6da9a87e520bc86a62cc27733687e054a1bd
Filipe Borges <filipebkc2209@gmail.com>
2019-10-19 20:03:12 -0300

    NumberUtil.js -> NumberUtil.ts

--- src/util/NumberUtils.ts
+++ src/util/NumberUtils.ts
@@ -1,8 +1,8 @@
-const getRandomNumber = (min, max) => (
+const getRandomNumber = (min: number, max: number) => (
   Math.random() * (max - min) + min
 );
 
-const normalizeNumberString = numberStr => (
+const normalizeNumberString = (numberStr: string) => (
   // eslint-disable-next-line no-useless-escape
   numberStr.replace(/[R$\s\.]/gi, '').replace('.', '').replace(',', '.')
 );
