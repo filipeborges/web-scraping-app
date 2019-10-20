5e620ac2850558fd600537269531c5cc0ad5ca70
Filipe Borges <filipebkc2209@gmail.com>
2019-10-20 16:42:17 -0300

    ResultProcessor.js -> ResultProcessor.ts

--- src/ResultProcessor.ts
+++ src/ResultProcessor.ts
@@ -1,11 +1,14 @@
 import { normalizeNumberString } from './util/NumberUtils';
 
 export default class ResultProcessor {
-  constructor(targetPrice) {
+
+  private targetPrice: number;
+
+  constructor(targetPrice: string) {
     this.targetPrice = Number(normalizeNumberString(targetPrice));
   }
 
-  isDesiredProductPrice(productPrice) {
+  isDesiredProductPrice(productPrice: string) {
     return productPrice
       && Number(normalizeNumberString(productPrice)) <= this.targetPrice;
   }
